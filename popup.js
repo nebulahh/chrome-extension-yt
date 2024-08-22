3cdfaf868605593037eb9cb2cca6acfa47c54bac
Lekan <olalekancrown11@gmail.com>
2024-08-22 12:01:42 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,51 @@
+import { getCurrentTab } from "./utils.js";
+
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    const bookmarkTitleElement = document.createElement('div')
+    const newBookmarkElement = document.createElement('div')
+
+    bookmarkTitleElement.textContent = bookmark.desc
+    bookmarkTitleElement.className = 'bookmark-title'
+
+    newBookmarkElement.id = 'bookmark-' + bookmark.time;
+    newBookmarkElement.className ='bookmark'
+    newBookmarkElement.setAttribute('timestamp', bookmark.time);
+
+    newBookmarkElement.appendChild(bookmarkTitleElement)
+    bookmarksElement.appendChild(newBookmarkElement)
+}
+
+const viewBookmarks = (currentBookmarks = []) => {
+    const bookmarksElement = document.getElementById('bookmarks')
+    bookmarksElement.innerHTML = ''
+
+    console.log({currentBookmarks});
+    
+    if (currentBookmarks.length > 0) {
+        for (let i = 0; i < currentBookmarks.length; i++) {
+            const bookmark = currentBookmarks[i];
+            addNewBookmark(bookmarksElement, bookmark)
+        }
+    } else {
+        bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>'
+    }
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const activeTab = await getCurrentTab();
+    const queryParameters = activeTab.url.split('?')[1];
+    const urlParameters = new URLSearchParams(queryParameters)
+
+    const currentVideo = urlParameters.get('v')
+
+    if (activeTab.url.includes('youtube.com/watch') && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+      
+            viewBookmarks(currentVideoBookmarks);
+          });
+    } else {
+        const container = document.getElementsByClassName('container')[0]
+        container.innerHTML = '<div class="title">This is not a youtube video.</div>';
+    }
+})
